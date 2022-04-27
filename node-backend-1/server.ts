203ffd4308f86061edcd05bb41ae0f6e20c94b09
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2022-04-27 15:42:37 -0400

    Adds node backend 1 to project

---
+++ node-backend-1/server.ts
@@ -0,0 +1,25 @@
+import Fastify from 'fastify';
+import cors from 'fastify-cors';
+
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.register(cors, {
+  origin: true // allow all cross-origin requests
+});
+
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' });
+});
+
+fastify.get('/api', function (request, reply) {
+  reply.send({ hello: 'world' });
+});
+
+fastify.listen(process.env.PORT || 3000, "0.0.0.0", function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
